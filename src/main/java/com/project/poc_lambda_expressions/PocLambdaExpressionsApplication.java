27e8704c0d72bdd8b5d7a4976f0dea0788db0eaa
ICAROCAETANO <icaro.caetano@gmail.com>
2026-08-06 22:02:42 -0300

    feat: removing spring references

--- src/main/java/com/project/poc_lambda_expressions/PocLambdaExpressionsApplication.java
+++ src/main/java/com/project/poc_lambda_expressions/PocLambdaExpressionsApplication.java
@@ -1,13 +1,9 @@
 package com.project.poc_lambda_expressions;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class PocLambdaExpressionsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocLambdaExpressionsApplication.class, args);
 	}
 
 }
