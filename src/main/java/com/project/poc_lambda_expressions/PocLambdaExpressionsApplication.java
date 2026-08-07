018ff7d567c3c49dcc195808041f5d5397b08a7f
ICAROCAETANO <icaro.caetano@gmail.com>
2026-08-06 22:01:25 -0300

    first commit

---
+++ src/main/java/com/project/poc_lambda_expressions/PocLambdaExpressionsApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_lambda_expressions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLambdaExpressionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLambdaExpressionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_lambda_expressions/PocLambdaExpressionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_lambda_expressions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLambdaExpressionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
